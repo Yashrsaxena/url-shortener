45b8b9efe70fbf1b155fbd212f6018aa6f5292e0
Yash Saxena <yashraj161999@gmail.com>
2024-10-28 20:29:16 +0530

    handleCreateNewShortURL writter

--- controllers/url.controller.js
+++ controllers/url.controller.js
@@ -5,17 +5,42 @@ const { randomUUID } = new ShortUniqueId({ length: 10 });
 
 // CREATE
 const handleCreateNewShortURL = async (req, res) => {
-  
+  const body = req.body;
+  if (body.id) {
+    let url = await Url.findOne({ shortId: body.id });
+    if (!url) {
+      url = new Url({
+        shortId: body.id,
+        redirectUrl: body.url,
+        name: body.name,
+      });
+      url.save();
+      return res.json({
+        Error_message: `Short Url unavailable, please select a unique one.`,
+      });
+    } else {
+      return res.status(201).json({
+        message: `Your short-id is ${url.shortId}`,
+      });
+    }
+  } else {
+    const id = randomUUID();
+    const url = new Url({
+      shortId: id,
+      redirectUrl: body.url,
+      name: body.name,
+    });
+    url.save();
+    return res.status(201).json({
+      message: `Your short-id is ${url.shortId}`,
+    });
+  }
 };
 
 // READ
-const handleGetAllURLs = async (req, res) => {
-  
-};
+const handleGetAllURLs = async (req, res) => {};
 
-const handleGetURLById = async (req, res) => {
-  
-};
+const handleGetURLById = async (req, res) => {};
 
 const handleGetAnalyticsById = (req, res) => {};
 
